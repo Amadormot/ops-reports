e281d87fe58c8eefc709a36b52a357d08e90ebe8
Pichau Mota <user@example.com>
2026-04-02 08:10:49 -0300

    fix: move migrations to startup and fix render.yaml schema

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,5 @@ set -o errexit
 # Instalação de dependências
 pip install -r requirements.txt
 
-# Migrações do banco de dados (crucial para o PostgreSQL)
-python manage.py migrate
-
 # Coleta de arquivos estáticos para o WhiteNoise
 python manage.py collectstatic --no-input
